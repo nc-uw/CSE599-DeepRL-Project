bd444f107548d8c5b0a7980a0b5c67cd34a656e8
Allen <allenc97@hotmail.com>
2018-06-03 15:06:39 -0700

    changes

--- Baseline/baseline.py
+++ Baseline/baseline.py
@@ -1,137 +1,94 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  2 00:07:29 2018
-
-@author: scsingh
-"""
 import numpy as np
-import math
 import random as rdm
 import matplotlib.pyplot as plt
+import math
+import sys
 
-def inverse_demand(d, saturation=90):
+def inverse_demand(d, saturation):
     p = saturation - d
     return p
 
-def table(options_pi, options_pj, mci):
-    N = {}    
-    for e_pj in options_pi:
-        N[e_pj] = {}
-        for action in options_pj:
-            d = e_pj + action
-            price = inverse_demand(d, saturation)
-            N[e_pj][action] =  price*action - mci*action
-    return N
-
-def state_estimate(options_pi, options_pj, mci):
-    N = {}    
-    for e_pj in options_pi:
-        N[e_pj] = {}
-        for action in options_pj:
-            d = e_pj + action
-            price = inverse_demand(d, saturation)
-            N[e_pj][action] =  price*action - mci*action
-    return N
+total_iters = 5000
+gamma = 0
+epsilon = 0.1
+alpha = 0.1
 
 
-'''         
-def cost(bid, params):
-    c = params[0]*bidˆˆ2 + params[1]*bid + params[2]
-    return c
-'''
+if __name__ == '__main__':
+    N = int(sys.argv[1])
+    mc = int(sys.argv[2])
+    min_quantity = int(sys.argv[3])
+    max_quantity = int(sys.argv[4])
 
-#cost (manual)
-mca = 25
-mcb = 25
-#mcb = 35
+    P = []
+    bandits = dict()
+    for i in range(N):
+        name = 'actions_' + chr(ord('a') + i)
+        bandits[name] = dict()
+        bandits[name]['actions'] = np.arange(min_quantity, max_quantity, 1.0)
+        bandits[name]['value_function'] = {action: 0 for action in bandits[name]['actions']}
+        bandits[name]['reward'] = []
+        bandits[name]['bids'] = []
+        bandits[name]['max_bids'] = []
 
-#choose pa, pb
-options_pa = [10., 12.5, 15, 17.5, 20., 22.5, 25, 27.5, 30]
-options_pb = [10., 12.5, 15, 17.5, 20., 22.5, 25, 27.5, 30]
-#options_pb = [6., 8., 10., 12., 14., 16., 18., 20]
+    for i in range(total_iters):
+        print('iter', i)
 
-#bid_pa = [rdm.choice(options_pa)]
-#bid_pb = [rdm.choice(options_pb)]
+        bids = []
+        for name in bandits:
+            bandit = bandits[name]
+            value_function = bandit['value_function']
+            max_bid = list(value_function.keys())[list(value_function.values()).index(max(value_function.values()))]
+            if rdm.uniform(0, 1) <= (1 - epsilon):
+                bid = max_bid
+                print("max bid", bid)
+            else:
+                bid = rdm.choice(bandit['actions'])
+                print("random bid", bid)
+            bandit['bids'].append(bid)
+            bandit['max_bids'].append(max_bid)
+            bids.append(bid)
 
-A = table(options_pa, options_pb, mca)
-B = table(options_pb, options_pa, mcb)
+        d = sum(bids)
+        #d = math.sqrt(bids[0] ** 2 + bids[1] ** 2 + bids[2] ** 2 + bids[3] ** 2)
+        saturation = 25 * N
+        p = inverse_demand(d, saturation)
+        print('price', p)
 
-BanditA = {}
-BanditA = {options:0 for options in options_pa}
-BanditB = {}
-BanditB = {options:0 for options in options_pb}
+        for name in bandits:
+            bandit = bandits[name]
+            bid = bandit['bids'][-1]
+            max_bid = bandit['max_bids'][-1]
+            reward = (p - mc) * bid
+            bandit['reward'].append(reward)
+            bandit['value_function'][bid] = (1 - alpha) * bandit['value_function'][bid] + alpha * (reward + gamma * max_bid)
 
-total_iters=2500
-gamma = 0.8
-epsilon = 0.975
-alpha = 0.5
-saturation = 90
+        epsilon = epsilon - epsilon * (float((i * 0.005)) / float(total_iters))
+        print('epsilon', epsilon)
+        P.append(p)
 
-RewA = []
-RewB = []
-BidA = []
-BidB = []
-P=[]
+    plt.figure()
+    plt.title('Reward trajectory for 4 n-armed bandits \nExploration: decaying, Alpha: 0.2, Discount Factor: 0.9')
+    for name in bandits:
+        plt.plot(bandits[name]['reward'], label=name)
+    plt.ylabel('Net Revenue')
+    plt.xlabel('Iterations')
+    plt.legend()
+    plt.show()
 
-for i in range(total_iters):
-    print ('iter', i)
-    max_bid_pa = list(BanditA.keys())[list(BanditA.values()).index(max(BanditA.values()))]
-    rndm_bid_pa = rdm.choice(options_pa)
-    if rdm.uniform(0,1) <= (epsilon):
-        bid_pa = max_bid_pa
-        print ("max A", bid_pa)
-    else:
-        bid_pa = rndm_bid_pa
-        print ("random A", bid_pa)
+    plt.figure()
+    plt.title('Bid trajectory for 4 n-armed bandits \nExploration: 0.975-greedy, Alpha: 0.2, Discount Factor: 0.9')
+    for name in bandits:
+        plt.plot(bandits[name]['bids'], label=name)
+    plt.ylabel('Qty Bid')
+    plt.xlabel('Iterations')
+    plt.legend()
+    plt.show()
 
-    max_bid_pb = list(BanditB.keys())[list(BanditB.values()).index(max(BanditB.values()))]
-    rndm_bid_pb = rdm.choice(options_pb)
-    if rdm.uniform(0,1) <= (epsilon):
-        bid_pb = max_bid_pb
-        print ("max B", bid_pb)
-    else:
-        bid_pb = rndm_bid_pb
-        print ("random B", bid_pb)
-    
-    d = bid_pa + bid_pb
-    p = inverse_demand(d,saturation)    
-    print ('price', p)
-    ra = p*bid_pa - mca*bid_pa
-    rb = p*bid_pb - mcb*bid_pb
-    
-    BanditA[bid_pa] = (1 - alpha)*BanditA[bid_pa] + alpha*(ra + gamma*max_bid_pa)
-    BanditB[bid_pb] = (1 - alpha)*BanditB[bid_pb] + alpha*(rb + gamma*max_bid_pb)
-    #epsilon = epsilon - epsilon*(float((i*0.025))/float(total_iters))
-    print ('epsilon', epsilon)
-    RewA.append(ra)
-    RewB.append(rb)
-    BidA.append(bid_pa)
-    BidB.append(bid_pb)
-    P.append(p)
-
-plt.figure()
-plt.title('Reward trajectory for 2 n-armed bandits \nExploration: 0.975-greedy, Alpha: 0.5, Discount Factor: 0.8')
-plt.plot(RewA,'r--', label = 'Agent A')
-plt.plot(RewB, 'b--', label = 'Agent B')
-plt.ylabel('Net Revenue')
-plt.xlabel('Iterations')
-plt.legend()
-#, RewB, 'bs')
-plt.show()
-
-plt.figure()
-plt.title('Bid trajectory for 2 n-armed bandits \nExploration: 0.975-greedy, Alpha: 0.5, Discount Factor: 0.8')
-plt.plot(BidA,'r--', label = 'Agent A')
-plt.plot(BidB, 'b--', label = 'Agent B')
-plt.ylabel('Qty Bid')
-plt.xlabel('Iterations')
-plt.legend()
-plt.show()
-        
-    
-    
-    
-            
-        
-
+    plt.figure()
+    plt.title('Price')
+    plt.plot(P, 'k--', label='Cournot Price')
+    plt.ylabel('Price')
+    plt.xlabel('Iterations')
+    plt.legend()
+    plt.show()
